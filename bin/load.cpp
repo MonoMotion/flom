141456274e3108b0aaa9f59e54860f426db3052c
coord.e <me@coord-e.com>
2018-12-12 14:56:57 +0000

    Add: Implement to save to file

--- bin/load.cpp
+++ bin/load.cpp
@@ -1,11 +1,11 @@
 #include <fstream>
-#include <stdexcept>
+#include <iomanip>
 
 #include "flom/flom.hpp"
 
 int main(int argc, char* argv[]) {
-  std::ifstream f(argv[1]);
+  std::ifstream f(argv[1], std::ios::binary);
   auto const motion = flom::import_legacy_json(f);
-
-  return 0;
+  std::ofstream o(argv[2], std::ios::trunc | std::ios::binary);
+  return motion.SerializeToOstream(&o);
 }
