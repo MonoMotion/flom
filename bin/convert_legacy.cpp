b4767e5e8276374be4aaa7f3e0d43a84cf695642
coord.e <me@coord-e.com>
2018-12-13 12:35:30 +0000

    Change: Rename `load` bin to `convert_legacy`

--- bin/convert_legacy.cpp
+++ bin/convert_legacy.cpp
@@ -4,6 +4,11 @@
 #include "flom/flom.hpp"
 
 int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " INPUT OUTPUT" << std::endl;
+    return -1;
+  }
+
   std::ifstream f(argv[1], std::ios::binary);
   auto const motion = flom::Motion::load_legacy_json(f);
   std::ofstream o(argv[2], std::ios::trunc | std::ios::binary);
