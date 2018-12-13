9324aee70677f1c169fae6c0b469d7f0b990a17b
coord.e <me@coord-e.com>
2018-12-13 11:41:19 +0000

    Add: Implement exdporting motion to protobuf

--- include/flom/motion.hpp
+++ include/flom/motion.hpp
@@ -35,10 +35,14 @@ private:
 namespace proto_util {
 
 boost::qvm::vec<double, 3> convert_vec3(proto::Vec3 const&);
+void convert_vec3(boost::qvm::vec<double, 3> const& vec, proto::Vec3* v_proto);
 Translation convert_translation(proto::Translation const&);
+void convert_translation(Translation const&, proto::Translation*);
 
 boost::qvm::quat<double> convert_quat(proto::Quaternion const&);
+void convert_quat(boost::qvm::quat<double> const& quat, proto::Quaternion* q_proto);
 Rotation convert_rotation(proto::Rotation const&);
+void convert_rotation(Rotation const&, proto::Rotation*);
 
 }
 
--- lib/motion.cpp
+++ lib/motion.cpp
@@ -1,5 +1,8 @@
 #include "flom/motion.hpp"
 
+#include <boost/qvm/quat_access.hpp>
+#include <boost/qvm/vec_access.hpp>
+
 namespace flom {
 
 Frame Motion::frame_at(double t) const {
@@ -64,13 +67,47 @@ void Motion::dump(std::ofstream& f) const {
 }
 
 proto::Motion Motion::to_protobuf() const {
-  throw std::runtime_error("Not implemented");
+  proto::Motion m;
+  m.set_model_id(this->model_id);
+  if(this->loop == LoopType::Wrap) {
+    m.set_loop(proto::Motion::Loop::Motion_Loop_Wrap);
+  } else if(this->loop == LoopType::None) {
+    m.set_loop(proto::Motion::Loop::Motion_Loop_None);
+  }
+  double last_t;
+  for(auto const& [t, frame] : this->frames) {
+    auto* frame_proto = m.add_frames();
+    frame_proto->set_duration(t - last_t);
+    last_t = t;
+    // TODO: Use <algorithm> (e.g. std::copy)
+    for (auto const& [joint, change] : frame.changes) {
+      (*frame_proto->mutable_changes())[joint] = change;
+    }
+    for (auto const& [link, effect] : frame.effects) {
+        proto::Effect e;
+        if (effect.translation) {
+          proto_util::convert_translation(*effect.translation, e.mutable_translation()->mutable_value());
+        }
+        if (effect.rotation) {
+          proto_util::convert_rotation(*effect.rotation, e.mutable_rotation()->mutable_value());
+        }
+        (*frame_proto->mutable_effects())[link] = e;
+    }
+  }
+
+  return std::move(m);
 }
 
 namespace proto_util {
 
 boost::qvm::vec<double, 3> convert_vec3(proto::Vec3 const& vec_proto) {
   return boost::qvm::vec<double, 3> { vec_proto.x(), vec_proto.y(), vec_proto.z() };
+}
+
+void convert_vec3(boost::qvm::vec<double, 3> const& vec, proto::Vec3* v_proto) {
+  v_proto->set_x(boost::qvm::X(vec));
+  v_proto->set_y(boost::qvm::Y(vec));
+  v_proto->set_z(boost::qvm::Z(vec));
 }
 
 Translation convert_translation(proto::Translation const& trans_proto) {
@@ -86,8 +123,24 @@ Translation convert_translation(proto::Translation const& trans_proto) {
   return std::move(trans);
 }
 
+void convert_translation(Translation const& trans, proto::Translation* trans_proto) {
+  if (trans.coord_system == CoordinateSystem::World) {
+    convert_vec3(trans.vec, trans_proto->mutable_world());
+  } else if (trans.coord_system == CoordinateSystem::Local) {
+     convert_vec3(trans.vec, trans_proto->mutable_local());
+  }
+  trans_proto->set_weight(trans.weight);
+}
+
 boost::qvm::quat<double> convert_quat(proto::Quaternion const& quat_proto) {
   return boost::qvm::quat<double> { quat_proto.w(), quat_proto.x(), quat_proto.y(), quat_proto.z() };
+}
+
+void convert_quat(boost::qvm::quat<double> const& quat, proto::Quaternion* q_proto) {
+  q_proto->set_w(boost::qvm::S(quat));
+  q_proto->set_x(boost::qvm::X(quat));
+  q_proto->set_y(boost::qvm::Y(quat));
+  q_proto->set_z(boost::qvm::Z(quat));
 }
 
 Rotation convert_rotation(proto::Rotation const& rot_proto) {
@@ -101,6 +154,15 @@ Rotation convert_rotation(proto::Rotation const& rot_proto) {
   }
   rot.weight = rot_proto.weight();
   return std::move(rot);
+}
+
+void convert_rotation(Rotation const& rot, proto::Rotation* rot_proto) {
+  if (rot.coord_system == CoordinateSystem::World) {
+     convert_quat(rot.quat, rot_proto->mutable_world());
+  } else if (rot.coord_system == CoordinateSystem::Local) {
+     convert_quat(rot.quat, rot_proto->mutable_local());
+  }
+  rot_proto->set_weight(rot.weight);
 }
 
 }
