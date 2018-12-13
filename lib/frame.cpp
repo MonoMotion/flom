ffc7e5f5de92d70e97e14ff7fa79abfb6bb77446
coord.e <me@coord-e.com>
2018-12-13 09:03:56 +0000

    Fix: Fix not to discard const qualifier

--- lib/frame.cpp
+++ lib/frame.cpp
@@ -5,12 +5,12 @@ namespace flom {
 Frame interpolate(double t, Frame const& a, Frame const& b) {
   Frame f;
   for (auto const& [k, v1] : a.changes) {
-    auto const v2 = b.changes[k];
-    f.changes.insert(k, lerp(t, v1, v2);
+    auto const v2 = b.changes.at(k);
+    f.changes[k] = lerp(t, v1, v2);
   }
   for (auto const& [k, v1] : a.effects) {
-    auto const v2 = b.effects[k];
-    f.effects.insert(k, interpolate(t, v1, v2);
+    auto const v2 = b.effects.at(k);
+    f.effects[k] = interpolate(t, v1, v2);
   }
   return std::move(f);
 }
