cc097615dc83b8a06124aa7b90a1bed5dcd84125
coord.e <me@coord-e.com>
2018-12-14 09:21:46 +0000

    Add: Add tests of effector interpolation

--- test/effector.cpp
+++ test/effector.cpp
@@ -4,6 +4,9 @@
 #include <rapidcheck.h>
 #include <rapidcheck/boost_test.h>
 
+#include <flom/effector.hpp>
+#include <flom/interpolation.hpp>
+
 #include "generators.hpp"
 
 BOOST_AUTO_TEST_SUITE(effector)
@@ -52,6 +55,28 @@ RC_BOOST_PROP(sub_rotation,
 
   auto e3 = e1 - e2;
   RC_ASSERT(e3.rotation->quat == e1.rotation->quat - e2.rotation->quat);
+}
+
+RC_BOOST_PROP(interpolation_location,
+              (const flom::Effector &e1, const flom::Effector &e2)) {
+  RC_PRE(e1.location && e2.location);
+
+  const double t = static_cast<double>(*rc::gen::inRange(0, 100)) / 100;
+
+  auto e3 = flom::interpolate(t, e1, e2);
+  auto l = flom::interpolate(t, *e1.location, *e2.location);
+  RC_ASSERT(e3.location->vec == l.vec);
+}
+
+RC_BOOST_PROP(interpolation_rotation,
+              (const flom::Effector &e1, const flom::Effector &e2)) {
+  RC_PRE(e1.rotation && e2.rotation);
+
+  const double t = static_cast<double>(*rc::gen::inRange(0, 100)) / 100;
+
+  auto e3 = flom::interpolate(t, e1, e2);
+  auto r = flom::interpolate(t, *e1.rotation, *e2.rotation);
+  RC_ASSERT(e3.rotation->quat == r.quat);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
