98afdd6e739b6dfa4822fbf2616be75ae60390fb
coord.e <me@coord-e.com>
2018-12-14 04:37:01 +0000

    Fix: Specify compile options per target via flom_set_compile_options

---
+++ cmake/compile-options.cmake
@@ -0,0 +1,13 @@
+function(flom_set_compile_options target)
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    target_compile_options(${target} PUBLIC -pedantic -Wall -Weverything -Wno-float-equal -Wno-padded -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-missing-prototypes -Wno-range-loop-analysis)
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    target_compile_options(${target} PUBLIC -pedantic -Wall -Wextra -Wno-float-equal -Wno-padded -Wno-unused-parameter -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-declarations -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wswitch-default -Wundef)
+  endif()
+
+  if(CONFIG STREQUAL "Debug")
+    target_compile_options(${target} PUBLIC -O0 -g3)
+  elseif(CONFIG STREQUAL "Release")
+    target_compile_options(${target} PUBLIC -O3)
+  endif()
+endfunction()
