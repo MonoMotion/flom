44219ee980507839b7e81cfba19efb233a1e86ad
coord.e <me@coord-e.com>
2018-12-14 08:41:25 +0000

    Add: Add more effector tests

--- test/effector.cpp
+++ test/effector.cpp
@@ -8,20 +8,50 @@
 
 BOOST_AUTO_TEST_SUITE(effector)
 
-RC_BOOST_PROP(mul_scalar_location,
-              (const from::Effector& e, double v)) {
+RC_BOOST_PROP(mul_scalar_location, (const flom::Effector &e, double v)) {
   RC_PRE(e.location);
 
   auto e2 = e * v;
   RC_ASSERT(e2.location->vec == e.location->vec * v);
 }
 
-RC_BOOST_PROP(mul_scalar_rotation,
-              (const from::Effector& e, double v)) {
+RC_BOOST_PROP(mul_scalar_rotation, (const flom::Effector &e, double v)) {
   RC_PRE(e.rotation);
 
   auto e2 = e * v;
   RC_ASSERT(e2.rotation->quat == e.rotation->quat * v);
+}
+
+RC_BOOST_PROP(add_location,
+              (const flom::Effector &e1, const flom::Effector &e2)) {
+  RC_PRE(e1.location && e2.location);
+
+  auto e3 = e1 + e2;
+  RC_ASSERT(e3.location->vec == e1.location->vec + e2.location->vec);
+}
+
+RC_BOOST_PROP(add_rotation,
+              (const flom::Effector &e1, const flom::Effector &e2)) {
+  RC_PRE(e1.rotation && e2.rotation);
+
+  auto e3 = e1 + e2;
+  RC_ASSERT(e3.rotation->quat == e1.rotation->quat + e2.rotation->quat);
+}
+
+RC_BOOST_PROP(sub_location,
+              (const flom::Effector &e1, const flom::Effector &e2)) {
+  RC_PRE(e1.location && e2.location);
+
+  auto e3 = e1 - e2;
+  RC_ASSERT(e3.location->vec == e1.location->vec - e2.location->vec);
+}
+
+RC_BOOST_PROP(sub_rotation,
+              (const flom::Effector &e1, const flom::Effector &e2)) {
+  RC_PRE(e1.rotation && e2.rotation);
+
+  auto e3 = e1 - e2;
+  RC_ASSERT(e3.rotation->quat == e1.rotation->quat - e2.rotation->quat);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
