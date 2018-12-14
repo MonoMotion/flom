e4116cdc95c3e044fafb2696439b8e98e60172be
coord.e <me@coord-e.com>
2018-12-14 01:41:13 +0000

    Add: Add constructors to Motion::Impl because LoopType is POD and must be initialized explicitly

--- lib/motion.cpp
+++ lib/motion.cpp
@@ -28,6 +28,11 @@ public:
   LoopType loop;
   std::map<double, Frame> raw_frames;
 
+  Motion::Impl(std::string const& model)
+    : model_id(model), loop(LoopType::None), raw_frames() {}
+  Motion::Impl()
+    : model_id(), loop(LoopType::None), raw_frames() {}
+
   static Motion from_protobuf(proto::Motion const&);
   proto::Motion to_protobuf() const;
 };
