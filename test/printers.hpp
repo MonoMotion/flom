062af7634f88795c4bf2bae2fb383fe09f83cc96
coord.e <me@coord-e.com>
2018-12-15 16:23:28 +0000

    Add: Add printers for flom classes

--- test/printers.hpp
+++ test/printers.hpp
@@ -1,6 +1,8 @@
 #ifndef FLOM_TEST_PRINTERS_HPP
 #define FLOM_TEST_PRINTERS_HPP
 
+#include <flom/flom.hpp>
+
 #include <boost/qvm/vec.hpp>
 #include <boost/qvm/quat.hpp>
 #include <boost/qvm/quat_access.hpp>
@@ -25,5 +27,51 @@ std::ostream &operator<<(std::ostream &os, const vec<T, Dim> &v) {
 }
 
 } // namespace boost::qvm
+
+namespace flom {
+
+std::ostream &operator<<(std::ostream &os, const Rotation &v) {
+  os << "rotation(" << v.weight << ", " << v.quat << ")";
+  return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const Location &v) {
+  os << "location(" << v.weight << ", " << v.vec << ")";
+  return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const Effector &v) {
+  os << "effector(";
+  if(v.location) {
+    os << *v.location << ",";
+  }
+  if(v.rotation) {
+    os << *v.rotation << ",";
+  }
+  os << ")";
+  return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const Frame &v) {
+  os << "frame(\n";
+  os << "positions(";
+  for(auto const& [j, p] : v.positions) {
+   os << j << ": " << p << ", ";
+  }
+  os << ")\neffectors(";
+  for(auto const& [l, e] : v.effectors) {
+   os << l << ": " << e << ", ";
+  }
+  os << "))";
+  return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const Motion &v) {
+  auto s = v.dump_json_string();
+  os << s;
+  return os;
+}
+
+}
 
 #endif
