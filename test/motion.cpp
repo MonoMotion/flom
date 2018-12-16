50ac49d34f24186240bb31fa6de574fa8d03bfae
coord.e <me@coord-e.com>
2018-12-16 16:16:42 +0000

    Add: Add tests of loop == None motions

--- test/motion.cpp
+++ test/motion.cpp
@@ -14,7 +14,7 @@
 
 BOOST_AUTO_TEST_SUITE(motion)
 
-RC_BOOST_PROP(retrieve_frame, (const flom::Motion &m, double t)) {
+RC_BOOST_PROP(retrieve_frame_wrap, (const flom::Motion &m, double t)) {
   RC_PRE(t >= 0);
   RC_PRE(m.loop() == flom::LoopType::Wrap);
   RC_PRE(m.is_valid());
@@ -25,6 +25,29 @@ RC_BOOST_PROP(retrieve_frame, (const flom::Motion &m, double t)) {
   auto expected_frame = m.frame_at(std::fmod(t, len)) + m.frame_at(len) * mul;
 
   RC_ASSERT(flom::almost_equal(frame, expected_frame));
+}
+
+RC_BOOST_PROP(retrieve_frame_none, (const flom::Motion &m, double t)) {
+  RC_PRE(t >= 0);
+  RC_PRE(m.length() >= t);
+  RC_PRE(m.loop() == flom::LoopType::None);
+  RC_PRE(m.is_valid());
+
+  auto len = m.length();
+  auto mul = static_cast<unsigned>(t / len);
+  auto frame = m.frame_at(t);
+  auto expected_frame = m.frame_at(std::fmod(t, len)) + m.frame_at(len) * mul;
+
+  RC_ASSERT(flom::almost_equal(frame, expected_frame));
+}
+
+RC_BOOST_PROP(retrieve_frame_none_throw, (const flom::Motion &m, double t)) {
+  RC_PRE(t >= 0);
+  RC_PRE(m.length() < t);
+  RC_PRE(m.loop() == flom::LoopType::None);
+  RC_PRE(m.is_valid());
+
+  RC_ASSERT_THROWS(m.frame_at(t));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
