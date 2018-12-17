3a733cf2fcae7d2e4fbccc1e6c3f18d1c2e55da1
coord.e <me@coord-e.com>
2018-12-17 14:28:21 +0000

    Add: Embed m.length() == 0 pattern in existing test cases

--- test/motion.cpp
+++ test/motion.cpp
@@ -20,13 +20,18 @@ BOOST_AUTO_TEST_SUITE(motion)
 RC_BOOST_PROP(retrieve_frame_wrap, (const flom::Motion &m, double t)) {
   RC_PRE(t >= 0);
   RC_PRE(m.loop() == flom::LoopType::Wrap);
-  RC_PRE(m.length() > 0);
   RC_PRE(m.is_valid());
 
-  auto len = m.length();
-  auto mul = static_cast<unsigned>(t / len);
   auto frame = m.frame_at(t);
-  auto expected_frame = m.frame_at(std::fmod(t, len)) + m.frame_at(len) * mul;
+
+  flom::Frame expected_frame;
+  if (m.length() == 0) {
+    expected_frame = m.frame_at(0);
+  } else {
+    auto len = m.length();
+    auto mul = static_cast<unsigned>(t / len);
+    expected_frame = m.frame_at(std::fmod(t, len)) + m.frame_at(len) * mul;
+  }
 
   // Using non-strict version of operator== defined in operators.hpp
   RC_ASSERT(frame == expected_frame);
@@ -36,13 +41,18 @@ RC_BOOST_PROP(retrieve_frame_none, (const flom::Motion &m, double t)) {
   RC_PRE(t >= 0);
   RC_PRE(m.length() >= t);
   RC_PRE(m.loop() == flom::LoopType::None);
-  RC_PRE(m.length() > 0);
   RC_PRE(m.is_valid());
 
-  auto len = m.length();
-  auto mul = static_cast<unsigned>(t / len);
   auto frame = m.frame_at(t);
-  auto expected_frame = m.frame_at(std::fmod(t, len)) + m.frame_at(len) * mul;
+
+  flom::Frame expected_frame;
+  if (m.length() == 0) {
+    expected_frame = m.frame_at(0);
+  } else {
+    auto len = m.length();
+    auto mul = static_cast<unsigned>(t / len);
+    expected_frame = m.frame_at(std::fmod(t, len)) + m.frame_at(len) * mul;
+  }
 
   // Using non-strict version of operator== defined in operators.hpp
   RC_ASSERT(frame == expected_frame);
