6ba5343a370e11712f6a555caae1c074471114ab
coord.e <me@coord-e.com>
2018-12-17 16:06:13 +0000

    Change: Disable warning flags to suppress warnings caused by Boost.Test macro expansions

--- cmake/test.cmake
+++ cmake/test.cmake
@@ -2,6 +2,7 @@ cmake_minimum_required(VERSION 3.0.2)
 
 function(flom_add_test target)
   target_link_libraries(${target} PRIVATE flom_lib rapidcheck)
+  target_compile_options(${target} PUBLIC -Wno-global-constructors -Wno-disabled-macro-expansion)
   add_test(NAME ${target} COMMAND ${target})
   enable_clang_format(${target})
   enable_clang_tidy(${target})
