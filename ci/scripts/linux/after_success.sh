4f95741b98814af59b4394704664f53a09167ae9
coord.e <me@coord-e.com>
2018-12-26 12:18:02 +0000

    Change: Do packaging in container

--- ci/scripts/linux/after_success.sh
+++ ci/scripts/linux/after_success.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
 docker run --rm -v $(pwd):/source -v $(pwd)/build:/build test /package.sh
-
-PREFIX=/usr
-VERSION=$(${TRAVIS_BUILD_DIR}/version.sh)
-INPUT=$(find ${TRAVIS_BUILD_DIR}/build/dist -name '*.tar.gz')
-OUTPUT=${INPUT%.tar.gz}
-
-function build_package() {
-  local type=$1
-  local ext=${2:-$type}
-  fpm -s tar -t $type -n flom -v $VERSION --prefix $PREFIX -p ${OUTPUT}.$ext $INPUT
-}
-
-build_package deb
-build_package rpm
-build_package pacman pkg.tar.xz
--- ci/test-image/package.sh
+++ ci/test-image/package.sh
@@ -2,8 +2,24 @@
 
 set -euo pipefail
 
+SOURCE_DIR=/source
 BUILD_DIR=/build
+DIST_DIR=./dist
 
 cd $BUILD_DIR
-cpack -D CPACK_OUTPUT_FILE_PREFIX=dist
+cpack -D CPACK_OUTPUT_FILE_PREFIX=$DIST_DIR
 
+PREFIX=/usr
+VERSION=$(./${SOURCE_DIR}/version.sh)
+INPUT=$(find $DIST_DIR -name '*.tar.gz')
+OUTPUT=${INPUT%.tar.gz}
+
+function build_package() {
+  local type=$1
+  local ext=${2:-$type}
+  fpm -s tar -t $type -n flom -v $VERSION --prefix $PREFIX -p ${OUTPUT}.$ext $INPUT
+}
+
+build_package deb
+build_package rpm
+build_package pacman pkg.tar.xz
