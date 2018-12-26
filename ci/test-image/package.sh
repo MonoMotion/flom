e35556b980d307f64d6adc5bd0c08e79a9fa74dc
coord.e <me@coord-e.com>
2018-12-26 14:50:09 +0000

    Fix: Strip archive before packaging

--- ci/test-image/package.sh
+++ ci/test-image/package.sh
@@ -11,13 +11,22 @@ cpack -D CPACK_OUTPUT_FILE_PREFIX=$DIST_DIR
 
 PREFIX=/usr
 VERSION=$(${SOURCE_DIR}/version.sh)
-INPUT=$(find $DIST_DIR -name '*.tar.gz')
-OUTPUT=${INPUT%.tar.gz}
+ARCHIVE=$(find $DIST_DIR -name '*.tar.gz')
+OUTPUT=${ARCHIVE%.tar.gz}
+INPUT=./artifact
+
+# TODO: This is not cool
+mkdir -p $INPUT && cd $INPUT
+tar --strip-components=1 -xf ../$ARCHIVE
+cd ..
 
 function build_package() {
   local type=$1
   local ext=${2:-$type}
-  fpm -s tar -t $type -n flom -v $VERSION -m "coord.e <me@coord-e.com>" --url "https://github.com/DeepL2/flom" --description "Motion data exchange format" --prefix $PREFIX -p ${OUTPUT}.$ext $INPUT
+
+  cd $INPUT
+  fpm -s dir -t $type -n flom -v $VERSION -m "coord.e <me@coord-e.com>" --url "https://github.com/DeepL2/flom" --description "Motion data exchange format" --prefix $PREFIX -p ../${OUTPUT}.$ext *
+  cd ..
 }
 
 build_package deb
