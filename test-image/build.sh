9badb7715273b9b0cb15df0584e6e42d7ecd3839
coord.e <me@coord-e.com>
2018-12-26 02:12:27 +0000

    Fix: Supply build directory externally

--- test-image/build.sh
+++ test-image/build.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
-mkdir /build && cd $_
+BUILD_DIR=$1
+
+mkdir -p $BUILD_DIR && cd $_
 cmake /source -DCMAKE_CXX_COMPILER=${CXX} -DCONFIG=${BUILD_TYPE} -DFORMAT_FILES_WITH_CLANG_FORMAT_BEFORE_EACH_BUILD=OFF -DCLANG_TIDY_ENABLE=OFF
 make -j"$(nproc)"
