5d7865e54b5bb0a628405dade9dd63bec1845c5e
coord.e <me@coord-e.com>
2018-12-26 10:28:32 +0000

    Add: Add a script and txt to manage version

---
+++ version.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+VERSION_TXT="VERSION.txt"
+
+cd "$(git rev-parse --show-toplevel)"
+
+function get_meta() {
+  local branch=$(git rev-parse --abbrev-ref HEAD)
+
+  if [ "$branch" == "master" ]; then
+    exit
+  fi
+
+  echo -n "+"
+
+  if [ "$TRAVIS_PULL_REQUEST" ]; then
+    echo "pr_$TRAVIS_PULL_REQUEST"
+  else
+    echo "$branch" | sed 's/[^[:alnum:]-]/-/g'
+  fi
+}
+
+function main() {
+  local base_version=$(cat $VERSION_TXT)
+  local meta_version=$(get_meta)
+
+  echo "${base_version}${meta_version}"
+}
+
+main
+
+
