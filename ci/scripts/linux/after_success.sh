cc9aa5ade178495b2d3674d425331c36384e0873
coord.e <me@coord-e.com>
2018-12-26 10:42:02 +0000

    Fix: Fix output path

--- ci/scripts/linux/after_success.sh
+++ ci/scripts/linux/after_success.sh
@@ -5,6 +5,6 @@ docker run --rm -v $(pwd):/source -v $(pwd)/build:/build test /package.sh
 PREFIX=/usr
 VERSION=$(${TRAVIS_BUILD_DIR}/version.sh)
 INPUT=$(find dist -name '*.tar.gz')
-OUTPUT=$(basename ${INPUT%.tar.gz})
+OUTPUT=${INPUT%.tar.gz}
 
 fpm -s tar -t deb -n flom -v $VERSION --prefix $PREFIX -p ${OUTPUT}.deb $INPUT
