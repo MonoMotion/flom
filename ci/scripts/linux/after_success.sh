0b90c9addc2b30ba84cfdc69890e1ff3fd1f920c
coord.e <me@coord-e.com>
2018-12-26 10:41:03 +0000

    Add: Build .deb package in CI

--- ci/scripts/linux/after_success.sh
+++ ci/scripts/linux/after_success.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 docker run --rm -v $(pwd):/source -v $(pwd)/build:/build test /package.sh
+
+PREFIX=/usr
+VERSION=$(${TRAVIS_BUILD_DIR}/version.sh)
+INPUT=$(find dist -name '*.tar.gz')
+OUTPUT=$(basename ${INPUT%.tar.gz})
+
+fpm -s tar -t deb -n flom -v $VERSION --prefix $PREFIX -p ${OUTPUT}.deb $INPUT
