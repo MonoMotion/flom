9ead9742eabb57379bb5c8d1a3af451aaa9fa77e
coord.e <me@coord-e.com>
2018-12-29 05:38:54 +0000

    Add: Add BaseError and inherit from that

--- include/flom/errors.hpp
+++ include/flom/errors.hpp
@@ -5,39 +5,48 @@
 
 namespace flom::errors {
 
-class InvalidTimeError : public std::runtime_error {
+class BaseError : public std::exception {
+public:
+  explicit BaseError(const std::string &) noexcept;
+  virtual const char *what() const noexcept;
+
+protected:
+  std::string message;
+};
+
+class InvalidTimeError : public BaseError {
 public:
   explicit InvalidTimeError(double);
   virtual const char *what() const noexcept;
 };
 
-class OutOfFramesError : public std::out_of_range {
+class OutOfFramesError : public BaseError {
 public:
   explicit OutOfFramesError(double);
   virtual const char *what() const noexcept;
 };
 
-class ParseError : public std::runtime_error {
+class ParseError : public BaseError {
 public:
   // TODO: include additional information
   ParseError();
   virtual const char *what() const noexcept;
 };
 
-class SerializationError : public std::runtime_error {
+class SerializationError : public BaseError {
 public:
   // TODO: include additional information
   SerializationError();
   virtual const char *what() const noexcept;
 };
 
-class JSONLoadError : public std::runtime_error {
+class JSONLoadError : public BaseError {
 public:
   explicit JSONLoadError(const std::string &);
   virtual const char *what() const noexcept;
 };
 
-class JSONDumpError : public std::runtime_error {
+class JSONDumpError : public BaseError {
 public:
   explicit JSONDumpError(const std::string &);
   virtual const char *what() const noexcept;
--- lib/errors.cpp
+++ lib/errors.cpp
@@ -25,45 +25,48 @@ namespace flom::errors {
 
 using namespace std::string_literals;
 
+BaseError::BaseError(const std::string &message_) noexcept
+    : message(message_) {}
+
+const char *BaseError::what() const noexcept { return this->message.c_str(); }
+
 InvalidTimeError::InvalidTimeError(double t)
-    : std::runtime_error("Time '" + std::to_string(t) + "' is invalid") {}
+    : BaseError("Time '" + std::to_string(t) + "' is invalid") {}
 
 const char *InvalidTimeError::what() const noexcept {
-  return ("InvalidTimeError: "s + std::runtime_error::what()).c_str();
+  return this->message.c_str();
 }
 
 OutOfFramesError::OutOfFramesError(double t)
-    : std::out_of_range("No frame is avaliable at time " + std::to_string(t)) {}
+    : BaseError("No frame is avaliable at time " + std::to_string(t)) {}
 
 const char *OutOfFramesError::what() const noexcept {
-  return ("OutOfFramesError: "s + std::out_of_range::what()).c_str();
+  return this->message.c_str();
 }
 
-ParseError::ParseError() : std::runtime_error("Could not parse input") {}
+ParseError::ParseError() : BaseError("Could not parse input") {}
 
-const char *ParseError::what() const noexcept {
-  return ("ParseError: "s + std::runtime_error::what()).c_str();
-}
+const char *ParseError::what() const noexcept { return this->message.c_str(); }
 
 SerializationError::SerializationError()
-    : std::runtime_error("Could not serialize data") {}
+    : BaseError("Could not serialize data") {}
 
 const char *SerializationError::what() const noexcept {
-  return ("SerializationError: "s + std::runtime_error::what()).c_str();
+  return this->message.c_str();
 }
 
 JSONLoadError::JSONLoadError(const std::string &message)
-    : std::runtime_error("Failed to load from JSON string: " + message) {}
+    : BaseError("Failed to load from JSON string: " + message) {}
 
 const char *JSONLoadError::what() const noexcept {
-  return ("JSONLoadError: "s + std::runtime_error::what()).c_str();
+  return this->message.c_str();
 }
 
 JSONDumpError::JSONDumpError(const std::string &message)
-    : std::runtime_error("Failed to dump to JSON string: " + message) {}
+    : BaseError("Failed to dump to JSON string: " + message) {}
 
 const char *JSONDumpError::what() const noexcept {
-  return ("JSONDumpError: "s + std::runtime_error::what()).c_str();
+  return this->message.c_str();
 }
 
 } // namespace flom::errors
