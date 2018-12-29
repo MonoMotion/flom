576f56c5eca270333934e1890a5c91049e55e5c7
coord.e <me@coord-e.com>
2018-12-29 06:16:10 +0000

    Change: Organize code

--- lib/range.cpp
+++ lib/range.cpp
@@ -3,6 +3,16 @@
 
 namespace flom {
 
+/* frame_iterator::Impl implementation */
+double frame_iterator::Impl::current_time() const noexcept {
+  return this->fps * this->t_index;
+}
+
+bool frame_iterator::Impl::check_is_end() const noexcept {
+  return !this->motion->is_in_range_at(this->current_time());
+}
+
+/* frame_iterator implementation */
 frame_iterator::frame_iterator() noexcept : impl(), is_end(true) {}
 frame_iterator::frame_iterator(Motion const &motion, double fps) noexcept
     : impl(std::make_unique<Impl>(motion, fps)) {}
@@ -42,16 +52,8 @@ frame_iterator frame_iterator::operator--(int) noexcept {
   return copy;
 }
 
-double frame_iterator::Impl::current_time() const noexcept {
-  return this->fps * this->t_index;
-}
-
 double frame_iterator::current_time() const noexcept {
   return this->impl->current_time();
-}
-
-bool frame_iterator::Impl::check_is_end() const noexcept {
-  return !this->motion->is_in_range_at(this->current_time());
 }
 
 frame_iterator::difference_type operator-(const frame_iterator &l,
