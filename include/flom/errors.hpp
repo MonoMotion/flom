e7d755dcf51b39db15a43f4bac2a8ecb5b23f005
coord.e <me@coord-e.com>
2018-12-29 12:34:36 +0000

    [WIP] Change; Inherit from std::exception and hold additional info in member

--- include/flom/errors.hpp
+++ include/flom/errors.hpp
@@ -5,42 +5,62 @@
 
 namespace flom::errors {
 
-class InvalidTimeError : public std::runtime_error {
+class InvalidTimeError : public std::exception {
 public:
   explicit InvalidTimeError(double);
   virtual const char *what() const noexcept;
+
+  double time() const noexcept;
+
+private:
+  double t;
 };
 
-class OutOfFramesError : public std::out_of_range {
+class OutOfFramesError : public std::exception {
 public:
   explicit OutOfFramesError(double);
   virtual const char *what() const noexcept;
+
+  double time() const noexcept;
+
+private:
+  double t;
 };
 
-class ParseError : public std::runtime_error {
+class ParseError : public std::exception {
 public:
   // TODO: include additional information
   ParseError();
   virtual const char *what() const noexcept;
 };
 
-class SerializationError : public std::runtime_error {
+class SerializationError : public std::exception {
 public:
   // TODO: include additional information
   SerializationError();
   virtual const char *what() const noexcept;
 };
 
-class JSONLoadError : public std::runtime_error {
+class JSONLoadError : public std::exception {
 public:
   explicit JSONLoadError(const std::string &);
   virtual const char *what() const noexcept;
+
+  std::string status() const noexcept;
+
+public:
+  std::string status;
 };
 
-class JSONDumpError : public std::runtime_error {
+class JSONDumpError : public std::exception {
 public:
   explicit JSONDumpError(const std::string &);
   virtual const char *what() const noexcept;
+
+  std::string status() const noexcept;
+
+public:
+  std::string status;
 };
 
 } // namespace flom::errors
