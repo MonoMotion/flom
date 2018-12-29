049dffed3fcb3be1dbc025a74d5413214d2850ec
coord.e <me@coord-e.com>
2018-12-29 03:04:21 +0000

    Change: Rewrire frame_iterator to be copyconstructible

--- include/flom/range.hpp
+++ include/flom/range.hpp
@@ -9,13 +9,6 @@
 
 namespace flom {
 
-// frame_iterator is based on random_generator_iterator.hpp
-// in https://github.com/yumetodo/random_generator_iterator,
-// which is distributed under the Boost Software License, Version 1.0.
-//
-// Copyright (C) 2016 yumetodo <yume-wikijp@live.jp>
-// (See http://www.boost.org/LICENSE_1_0.txt)
-//
 // using snake_case, following customs of iterator naming
 class frame_iterator {
 public:
@@ -26,48 +19,25 @@ public:
   using reference = Frame &;
 
 private:
-  struct Impl {
-    std::reference_wrapper<const Motion> motion;
-    double fps;
-    long t_index;
-    bool next_is_end;
-    Impl() = default;
-    Impl(const Impl &) = delete;
-    Impl(Impl &&) = default;
-    Impl &operator=(const Impl &) = delete;
-    Impl &operator=(Impl &&) = default;
-    Impl(Motion const &motion_, double fps_)
-        : motion(motion_), fps(fps_), t_index(0), next_is_end(false) {}
-    value_type get() {
-      return this->motion.get().frame_at(this->fps * this->t_index);
-    }
-    bool check_bound() const {
-      return this->motion.get().is_in_range_at(this->fps * this->t_index);
-    }
-  };
-
-  std::unique_ptr<Impl> pimpl;
+  // DefaultConstructible is required
+  const Motion *motion;
   bool is_end_iterator;
+  double fps = 1;
+  long t_index = 0;
 
 public:
-  constexpr frame_iterator() noexcept : pimpl(), is_end_iterator(true) {}
-  frame_iterator(const frame_iterator &) = delete;
-  frame_iterator(frame_iterator &&) = default;
-  frame_iterator &operator=(const frame_iterator &) = delete;
-  frame_iterator &operator=(frame_iterator &&) = default;
+  constexpr frame_iterator() noexcept : motion(), is_end_iterator(true) {}
+  frame_iterator(Motion const &motion, double fps) noexcept
+      : motion(&motion), fps(fps), is_end_iterator(false) {}
 
-  frame_iterator(Motion const &motion, double fps)
-      : pimpl(std::make_unique<Impl>(motion, fps)), is_end_iterator(false) {}
-
-  void stop() noexcept { this->pimpl->next_is_end = true; }
-
-  value_type operator*() { return this->pimpl->get(); }
+  value_type operator*() {
+    return this->motion->frame_at(this->fps * this->t_index);
+  }
 
   frame_iterator &operator++() noexcept {
-    this->pimpl->t_index++;
-    this->pimpl->next_is_end = !this->pimpl->check_bound();
-    if (this->pimpl->next_is_end)
-      this->is_end_iterator = true;
+    this->t_index++;
+    this->is_end_iterator =
+        !this->motion->is_in_range_at(this->fps * this->t_index);
     return *this;
   }
 
