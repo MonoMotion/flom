6f82c126be9d9a7b21a7ac2bb20bbba859bdf063
coord.e <me@coord-e.com>
2018-12-30 04:28:14 +0000

    Add: Add a comment that describes what version.sh does

--- version.sh
+++ version.sh
@@ -17,6 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Flom.  If not, see <http://www.gnu.org/licenses/>.
 #
+# This script prints semver-compatible version string calculated from current state of git
 
 set -euo pipefail
 
