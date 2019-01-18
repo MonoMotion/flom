eba8c3ff5449f75526cbc23dcb000b2c56abbc39
coord.e <me@coord-e.com>
2019-01-18 07:33:14 +0000

    Add: Organize library setting into flom_add_lib

---
+++ cmake/lib.cmake
@@ -0,0 +1,28 @@
+#
+# Copyright 2018 coord.e
+#
+# This file is part of Flom.
+#
+# Flom is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Flom is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Flom.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+function(flom_add_lib target name)
+  flom_set_compile_options(${target})
+  set_target_properties(${target} PROPERTIES OUTPUT_NAME "${name}")
+  set_target_properties(${target} PROPERTIES POSITION_INDEPENDENT_CODE TRUE)
+  target_link_libraries(${target} PRIVATE flom_proto nlohmann_json::nlohmann_json ${PROTOBUF_LIBRARIES})
+  enable_clang_format(${target})
+  enable_clang_tidy(${target})
+  install(TARGETS ${target} LIBRARY DESTINATION lib)
+endfunction()
