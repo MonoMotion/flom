4842c0b09654ee703b23789aab11cdfbe6337ce6
coord.e <me@coord-e.com>
2019-01-27 09:11:27 +0000

    Change: Implement loose comparions for tests in operators.hpp

--- test/operators.hpp
+++ test/operators.hpp
@@ -23,32 +23,85 @@
 #include <flom/frame.hpp>
 #include <flom/effector.hpp>
 
-// Non-strict == operators to use in tests.
+// loose comparison functions to use in tests.
 
 namespace flom {
 
-bool operator==(const FrameDifference& v1, const FrameDifference& v2) {
-  return almost_equal(v1, v2);
+namespace testing {
+
+static bool almost_equal(const FrameDifference& v1, const FrameDifference& v2) {
+  auto p = std::all_of(std::cbegin(v1.positions()), std::cend(v1.positions()),
+                       [&v2](auto const &pair) {
+                         auto const &[joint, pos1] = pair;
+                         auto const pos2 = v2.positions().at(joint);
+                         return almost_equal(pos1, pos2);
+                       });
+  auto e = std::all_of(std::cbegin(v1.effectors()), std::cend(v1.effectors()),
+                       [&v2](auto const &pair) {
+                         auto const &[link, e1] = pair;
+                         auto const e2 = v2.effectors().at(link);
+                         return almost_equal(e1, e2);
+                       });
+  return p && e;
 }
 
-bool operator==(const EffectorDifference& v1, const EffectorDifference& v2) {
-  return almost_equal(v1, v2);
+static bool almost_equal(const EffectorDifference& v1, const EffectorDifference& v2) {
+  const bool l =
+      static_cast<bool>(v1.location()) == static_cast<bool>(v2.location());
+  const bool r =
+      static_cast<bool>(v1.rotation()) == static_cast<bool>(v2.rotation());
+  if (!l || !r)
+    return false;
+  bool result = true;
+  if (v1.location())
+    result = result && almost_equal(*v1.location(), *v2.location());
+  if (v1.rotation())
+    result = result && almost_equal(*v1.rotation(), *v2.rotation());
+  return result;
 }
 
-bool operator==(const Frame& v1, const Frame& v2) {
-  return almost_equal(v1, v2);
+static bool almost_equal(const Frame& v1, const Frame& v2) {
+  auto p = std::all_of(std::cbegin(v1.positions()), std::cend(v1.positions()),
+                       [&v2](auto const &pair) {
+                         auto const &[joint, pos1] = pair;
+                         auto const pos2 = v2.positions().at(joint);
+                         return almost_equal(pos1, pos2);
+                       });
+  auto e = std::all_of(std::cbegin(v1.effectors()), std::cend(v1.effectors()),
+                       [&v2](auto const &pair) {
+                         auto const &[link, e1] = pair;
+                         auto const e2 = v2.effectors().at(link);
+                         return almost_equal(e1, e2);
+                       });
+  return p && e;
 }
 
-bool operator==(const Effector& v1, const Effector& v2) {
-  return almost_equal(v1, v2);
+static bool almost_equal(const Effector& v1, const Effector& v2) {
+  // TODO: Refactor: Remove mutable variable
+  const bool l =
+      static_cast<bool>(v1.location()) == static_cast<bool>(v2.location());
+  const bool r =
+      static_cast<bool>(v1.rotation()) == static_cast<bool>(v2.rotation());
+  if (!l || !r)
+    return false;
+  bool result = true;
+  if (v1.location())
+    result = result && almost_equal(*v1.location(), *v2.location());
+  if (v1.rotation())
+    result = result && almost_equal(*v1.rotation(), *v2.rotation());
+  return result;
 }
 
-bool operator==(const Location& v1, const Location& v2) {
-  return almost_equal(v1, v2);
+static bool almost_equal(const Location& v1, const Location& v2) {
+  return boost::qvm::cmp(v1.vector(), v2.vector(),
+                         [](auto e1, auto e2) { return loose_compare(e1, e2); });
 }
 
-bool operator==(const Rotation& v1, const Rotation& v2) {
-  return almost_equal(v1, v2);
+static bool almost_equal(const Rotation& v1, const Rotation& v2) {
+  return boost::qvm::cmp(v1.quaternion(), v2.quaternion(),
+                         [](auto e1, auto e2) { return loose_compare(e1, e2); });
+}
+
 }
 
 }
