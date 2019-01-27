44e0435878f7508880c7e343dc79845e306ef4b1
coord.e <me@coord-e.com>
2019-01-27 10:02:07 +0000

    Fix: Fix some to be compiled successfully

--- test/operators.hpp
+++ test/operators.hpp
@@ -22,6 +22,17 @@
 
 #include <flom/frame.hpp>
 #include <flom/effector.hpp>
+#include <flom/loose_compare.hpp>
+
+#include <rapidcheck.h>
+
+#include <boost/qvm/vec.hpp>
+#include <boost/qvm/vec_traits_array.hpp>
+#include <boost/qvm/vec_access.hpp>
+#include <boost/qvm/vec_operations.hpp>
+#include <boost/qvm/quat.hpp>
+#include <boost/qvm/quat_access.hpp>
+#include <boost/qvm/quat_operations.hpp>
 
 // loose comparison functions to use in tests.
 
@@ -29,23 +40,30 @@ namespace flom {
 
 namespace testing {
 
-static bool almost_equal(const FrameDifference& v1, const FrameDifference& v2) {
-  auto p = std::all_of(std::cbegin(v1.positions()), std::cend(v1.positions()),
-                       [&v2](auto const &pair) {
-                         auto const &[joint, pos1] = pair;
-                         auto const pos2 = v2.positions().at(joint);
-                         return almost_equal(pos1, pos2);
-                       });
-  auto e = std::all_of(std::cbegin(v1.effectors()), std::cend(v1.effectors()),
-                       [&v2](auto const &pair) {
-                         auto const &[link, e1] = pair;
-                         auto const e2 = v2.effectors().at(link);
-                         return almost_equal(e1, e2);
-                       });
-  return p && e;
+static bool almost_equal(double a, double b) {
+  return loose_compare(a, b);
 }
 
-static bool almost_equal(const EffectorDifference& v1, const EffectorDifference& v2) {
+static bool almost_equal(const qvm::vec<double, 3>& a, const qvm::vec<double, 3>& b) {
+  return boost::qvm::cmp(a, b,
+                         [](auto e1, auto e2) { return almost_equal(e1, e2); });
+}
+
+static bool almost_equal(const qvm::quat<double>& a, const qvm::quat<double>& b) {
+  return boost::qvm::cmp(a, b,
+                         [](auto e1, auto e2) { return almost_equal(e1, e2); });
+}
+
+static bool almost_equal(const Location& v1, const Location& v2) {
+  return almost_equal(v1.vector(), v2.vector());
+}
+
+static bool almost_equal(const Rotation& v1, const Rotation& v2) {
+  return almost_equal(v1.quaternion(), v2.quaternion());
+}
+
+static bool almost_equal(const Effector& v1, const Effector& v2) {
+  // TODO: Refactor: Remove mutable variable
   const bool l =
       static_cast<bool>(v1.location()) == static_cast<bool>(v2.location());
   const bool r =
@@ -76,8 +94,7 @@ static bool almost_equal(const Frame& v1, const Frame& v2) {
   return p && e;
 }
 
-static bool almost_equal(const Effector& v1, const Effector& v2) {
-  // TODO: Refactor: Remove mutable variable
+static bool almost_equal(const EffectorDifference& v1, const EffectorDifference& v2) {
   const bool l =
       static_cast<bool>(v1.location()) == static_cast<bool>(v2.location());
   const bool r =
