48ec6355e74f5474457db51b81e6080d2aa092ce
coord.e <me@coord-e.com>
2019-01-27 10:21:07 +0000

    Change: Seperate implementation of almost_equal

--- cmake/test.cmake
+++ cmake/test.cmake
@@ -20,7 +20,7 @@
 cmake_minimum_required(VERSION 3.0.2)
 
 function(flom_add_test target)
-  target_link_libraries(${target} PRIVATE flom_lib rapidcheck)
+  target_link_libraries(${target} PRIVATE flom_lib flom_test_lib rapidcheck)
   add_dependencies(${target} flom_test_headers)
   if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     target_compile_options(${target} PUBLIC -Wno-global-constructors -Wno-disabled-macro-expansion)
--- test/include/comparison.hpp
+++ test/include/comparison.hpp
@@ -22,17 +22,11 @@
 
 #include <flom/effector.hpp>
 #include <flom/frame.hpp>
-#include <flom/loose_compare.hpp>
 
 #include <rapidcheck.h>
 
 #include <boost/qvm/quat.hpp>
-#include <boost/qvm/quat_access.hpp>
-#include <boost/qvm/quat_operations.hpp>
 #include <boost/qvm/vec.hpp>
-#include <boost/qvm/vec_access.hpp>
-#include <boost/qvm/vec_operations.hpp>
-#include <boost/qvm/vec_traits_array.hpp>
 
 // loose comparison functions to use in tests.
 
@@ -40,91 +34,19 @@ namespace flom {
 
 namespace testing {
 
-static bool almost_equal(double a, double b) { return loose_compare(a, b); }
+bool almost_equal(double, double);
 
-static bool almost_equal(const qvm::vec<double, 3> &a,
-                         const qvm::vec<double, 3> &b) {
-  return boost::qvm::cmp(a, b,
-                         [](auto e1, auto e2) { return almost_equal(e1, e2); });
-}
+bool almost_equal(const qvm::vec<double, 3> &, const qvm::vec<double, 3> &);
+bool almost_equal(const qvm::quat<double> &, const qvm::quat<double> &);
 
-static bool almost_equal(const qvm::quat<double> &a,
-                         const qvm::quat<double> &b) {
-  return boost::qvm::cmp(a, b,
-                         [](auto e1, auto e2) { return almost_equal(e1, e2); });
-}
+bool almost_equal(const Location &, const Location &);
+bool almost_equal(const Rotation &, const Rotation &);
 
-static bool almost_equal(const Location &v1, const Location &v2) {
-  return almost_equal(v1.vector(), v2.vector());
-}
+bool almost_equal(const Effector &, const Effector &);
+bool almost_equal(const Frame &, const Frame &);
 
-static bool almost_equal(const Rotation &v1, const Rotation &v2) {
-  return almost_equal(v1.quaternion(), v2.quaternion());
-}
-
-static bool almost_equal(const Effector &v1, const Effector &v2) {
-  // TODO: Refactor: Remove mutable variable
-  const bool l =
-      static_cast<bool>(v1.location()) == static_cast<bool>(v2.location());
-  const bool r =
-      static_cast<bool>(v1.rotation()) == static_cast<bool>(v2.rotation());
-  if (!l || !r)
-    return false;
-  bool result = true;
-  if (v1.location())
-    result = result && almost_equal(*v1.location(), *v2.location());
-  if (v1.rotation())
-    result = result && almost_equal(*v1.rotation(), *v2.rotation());
-  return result;
-}
-
-static bool almost_equal(const Frame &v1, const Frame &v2) {
-  auto p = std::all_of(std::cbegin(v1.positions()), std::cend(v1.positions()),
-                       [&v2](auto const &pair) {
-                         auto const &[joint, pos1] = pair;
-                         auto const pos2 = v2.positions().at(joint);
-                         return almost_equal(pos1, pos2);
-                       });
-  auto e = std::all_of(std::cbegin(v1.effectors()), std::cend(v1.effectors()),
-                       [&v2](auto const &pair) {
-                         auto const &[link, e1] = pair;
-                         auto const e2 = v2.effectors().at(link);
-                         return almost_equal(e1, e2);
-                       });
-  return p && e;
-}
-
-static bool almost_equal(const EffectorDifference &v1,
-                         const EffectorDifference &v2) {
-  const bool l =
-      static_cast<bool>(v1.location()) == static_cast<bool>(v2.location());
-  const bool r =
-      static_cast<bool>(v1.rotation()) == static_cast<bool>(v2.rotation());
-  if (!l || !r)
-    return false;
-  bool result = true;
-  if (v1.location())
-    result = result && almost_equal(*v1.location(), *v2.location());
-  if (v1.rotation())
-    result = result && almost_equal(*v1.rotation(), *v2.rotation());
-  return result;
-}
-
-static bool almost_equal(const FrameDifference &v1, const FrameDifference &v2) {
-  auto p = std::all_of(std::cbegin(v1.positions()), std::cend(v1.positions()),
-                       [&v2](auto const &pair) {
-                         auto const &[joint, pos1] = pair;
-                         auto const pos2 = v2.positions().at(joint);
-                         return almost_equal(pos1, pos2);
-                       });
-  auto e = std::all_of(std::cbegin(v1.effectors()), std::cend(v1.effectors()),
-                       [&v2](auto const &pair) {
-                         auto const &[link, e1] = pair;
-                         auto const e2 = v2.effectors().at(link);
-                         return almost_equal(e1, e2);
-                       });
-  return p && e;
-}
+bool almost_equal(const EffectorDifference &, const EffectorDifference &);
+bool almost_equal(const FrameDifference &, const FrameDifference &);
 
 } // namespace testing
 
