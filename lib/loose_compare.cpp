b7e6121c6aae2caef9fcc6b04e4abd89f4ab07d6
coord.e <me@coord-e.com>
2019-01-27 08:43:47 +0000

    Add: Add loose_compare(double, double)

---
+++ lib/loose_compare.cpp
@@ -0,0 +1,31 @@
+//
+// Copyright 2018 coord.e
+//
+// This file is part of Flom.
+//
+// Flom is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Flom is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Flom.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "flom/loose_compare.hpp"
+
+#include <boost/test/floating_point_comparison.hpp>
+
+namespace flom {
+
+bool loose_compare(double a, double b) {
+  return boost::math::fpc::close_at_tolerance<double>(
+      constants::float_point_tolerance)(a, b);
+}
+
+} // namespace flom
