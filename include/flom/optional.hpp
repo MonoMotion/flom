c82179ddc7768d7ba3afd29b9adbf402d2c7736c
coord.e <me@coord-e.com>
2019-02-04 05:16:03 +0000

    Revert "Add: Add a comment describing the situation"
    This reverts commit 93d5a90eeb03495c0c514c645e553ab19912c0ca.

--- include/flom/optional.hpp
+++ include/flom/optional.hpp
@@ -27,19 +27,6 @@ namespace flom::optional {
 static inline auto nullopt = compat::nullopt;
 using nullopt_t = decltype(nullopt);
 
-//
-// a class template for an optional contained value of T
-//
-// expected to work as if this is an alias of std::optional
-//
-// Background:
-// the real signature of compat::optional changes
-// depending on the language processor.
-// If libflom.so is compiled with std::optional,
-// that cannot be linked with libflom.so with boost::optional.
-// With this class, signature is always `flom::optional::optional`
-// and linking failure cannot be happen.
-//
 template <typename T> class optional : public compat::optional<T> {
 public:
   using base_type = compat::optional<T>;
